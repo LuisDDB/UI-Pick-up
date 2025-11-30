02322a4b9e5c7764c873e7c57c91d9400053a96b
LuisDDB <Luisddb1706@gmail.com>
2025-11-29 18:14:55 -0700

    Se corrigio un error con el closeModal

--- login/login.js
+++ login/login.js
@@ -12,7 +12,7 @@ class Login extends HTMLElement {
         <div id="modal" class="modal">
             <form id="loginForm" class="form-login">
                 <div class="form-header">
-                    <button class="btn close-btn">X</button>
+                    <button type="button" class="btn close-btn">X</button>
                 </div>
                 <div class="center">
                     <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-user"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
@@ -31,6 +31,7 @@ class Login extends HTMLElement {
         </div>
         `;
 
+        const componentInstance = this;
         const formLogin = document.getElementById("loginForm");
         formLogin.addEventListener("submit", async e => {
             e.preventDefault();
@@ -54,6 +55,7 @@ class Login extends HTMLElement {
                 localStorage.setItem("token", data.token);
 
                 location.hash = "#/home";
+                closeModal();
             } catch (error) {
                 console.error(error);
                 alert("Error al conectar con el servidor");
@@ -74,7 +76,7 @@ class Login extends HTMLElement {
         });
 
         function closeModal() {
-            modal.remove()
+            componentInstance.remove()
         }
 
         
