bd7c9f02d4a403816eab5840526dcfa95fc46b22
LuisDDB <Luisddb1706@gmail.com>
2025-12-04 13:11:35 -0700

    Se cambio para mostrar por el pahtname en lugar del #

--- index/router.js
+++ index/router.js
@@ -3,12 +3,23 @@ import { environment } from "./config/environment.js";
 export async function router() {
     
     const app = document.getElementById("app");
-    const auth = document.getElementById("auth")
-    const hash = location.hash;
 
-    
+    const path = window.location.pathname; 
+    console.log("Ruta actual:", path);
 
-
-   
     app.innerHTML = "";
+
+    switch (path) {
+        case "/":
+            app.innerHTML = "<h1>Home</h1>";
+            break;
+
+        case "/pedidos":
+            app.innerHTML = "<h1>Pedidos</h1>";
+            break;
+
+        default:
+            app.innerHTML = "<h1>404 - No encontrado</h1>";
+            break;
+    }
 }
