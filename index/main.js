e435db0caa2c962fb7fe5e4b335908397aff9c1d
LuisDDB <Luisddb1706@gmail.com>
2025-12-04 17:19:23 -0700

    Se movio toda la logica al webcomponent Header

--- index/main.js
+++ index/main.js
@@ -1,43 +1,9 @@
-import { router } from "./router.js";
-
-document.addEventListener("click", (e) => {
-    if (e.target.matches("[data-route]")) {
-        e.preventDefault();
-        const url = e.target.getAttribute("href");
-
-        window.history.pushState({}, "", url);
-
-        router();
-    }
-});
-
-window.addEventListener("popstate", router);
 
 
-const btnLogin = document.getElementById("login-btn");
-btnLogin.addEventListener("click", ()=>{
-    openModal("mfe-login");
-});
-
-const btnRegister = document.getElementById("register-btn");
-btnRegister.addEventListener("click", ()=>{
-    openModal("mfe-register");
-});
-
-document.addEventListener("open-login",()=>{
-    openModal("mfe-login");
-})
-
-document.addEventListener("open-register", ()=>{
-    openModal("mfe-register");
-});
-
-function openModal(etiquta){
-    const modal = document.createElement(etiquta);
-    document.body.appendChild(modal);
-}
 
 
+const user = JSON.parse(localStorage.getItem("user"));
+console.log("Main ",user);
 
 
 
